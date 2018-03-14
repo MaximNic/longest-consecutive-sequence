18f449dad43f3650d6e12b079767da0285b18d9d
MaximNic <maxlabnk@gmail.com>
2018-03-14 22:24:14 +0300

    df

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function longestConsecutiveLength(array) {
+
+ function compareNumbers(a, b) {
+    return a - b;
+  }
+  array.sort(compareNumbers);
+  var longestCons=1;                                  // longest sequence
+  var seq=1;                                          // current sequence
+  for (let i=0; i<array.length; i++){                   
+    if (array[i] != array[i+1]){                // если не дубль
+      if (array[i]==array[i+1]-1){                //если следующий элемене на 1 больше текущего
+        seq++;                                    // текущий счет +1
+                                           //  console.log(seq,' ++    elem-',array[i],'-',array[i+1],'    longest-',longestCons);
+      } else {        
+        if (seq>longestCons){                    // если текущее больше максимального          
+                                      //  console.log(seq,' longest-',longestCons);
+          longestCons=seq;                 
+          
+        }
+        seq=1;                        //  console.log(' longest-',longestCons);
+      }
+    }else {
+      //console.log('  para-',array[i]);
+    }
+  }
+  //console.log(array,' longest-',longestCons);
+  
+ return longestCons;
+  
   // your solution here
 }
